84cfa2f99e22a0794eb4a53338c52e4ba4cc1f7b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-08-23 18:31:10 +0600

    take note about global execution context

---
+++ js/Global Execution Context/index.js
@@ -0,0 +1,8 @@
+//! 1
+// console.log(a);
+// var a = 10; // Output: undefined  // here first set all the variable in the memory as undefined and then it read all code from top to bottom , we are declaring a after the console that why in the console we get undefined because the variable is hoisted to the top of the scope but not the value assigned to it.
+
+//! 2
+// a = 10;
+// console.log(a);  // Output: 10  // after storing the variable when it read the code from first line it find the variable a and assign the value to it and then print it in the console.
+// var a= 20;
